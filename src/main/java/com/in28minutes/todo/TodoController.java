8713cbc3d2c956b2113e05bb96456b49ecfdcc88
Gabriel Müller <gabrielgoetzmuller@gmail.com>
2021-04-21 15:40:33 -0300

    TodoController and TodoService implemented
    Simple Todo structure to start developing new features for it. LoginController  was removed, for now.
    New list-todos jsp view.

---
+++ src/main/java/com/in28minutes/todo/TodoController.java
@@ -0,0 +1,22 @@
+package com.in28minutes.todo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class TodoController {
+	
+		@Autowired
+		TodoService service;
+
+		@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
+		public String showLoginPage(ModelMap model) {
+			model.addAttribute("todos", service.retrieveTodos("in28Minutes"));
+			return "list-todos";
+		}
+}
