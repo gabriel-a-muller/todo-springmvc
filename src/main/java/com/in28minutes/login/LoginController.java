87c1b26eb3aeef1eea9ebcd79723375287fcd6ae
Gabriel Müller <gabrielgoetzmuller@gmail.com>
2021-04-21 03:48:44 -0300

    Spring Autowiring and Dependency Injection

--- src/main/java/com/in28minutes/login/LoginController.java
+++ src/main/java/com/in28minutes/login/LoginController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
-		UserValidationService service = new UserValidationService(); 
+		@Autowired
+		UserValidationService service;
 
 		@RequestMapping(value = "/login", method = RequestMethod.GET)
 		/*@ResponseBody*/
--- src/main/java/com/in28minutes/login/UserValidationService.java
+++ src/main/java/com/in28minutes/login/UserValidationService.java
@@ -1,9 +1,9 @@
 package com.in28minutes.login;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserValidationService {
-	
-	private static final long serialVersionUID = 1L;
-	
 	public boolean isUserValid(String user, String password) {
 		return user.equals("Gabriel") 
 				&& password.equals("123123");
