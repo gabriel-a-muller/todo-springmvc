a7e5b7a60a6f842db350a21da3f329001f449d96
Gabriel Müller <gabrielgoetzmuller@gmail.com>
2021-04-26 17:17:15 -0300

    Implemented Hibernate Validators
    Usage of @Size, @Valid and BindingResult

--- src/main/java/com/in28minutes/todo/TodoController.java
+++ src/main/java/com/in28minutes/todo/TodoController.java
@@ -2,9 +2,12 @@ package com.in28minutes.todo;
 
 import java.util.Date;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,7 +27,7 @@ public class TodoController {
 			model.addAttribute("todos", service.retrieveTodos("Gabriel"));
 			return "list-todos";
 		}
-		
+
 		@RequestMapping(value = "/add-todo", method = RequestMethod.GET)
 		public String addTodoPage(ModelMap model) {
 			model.addAttribute("todo", new Todo(0, "Gabriel", "", new Date(), false));
@@ -32,7 +35,11 @@ public class TodoController {
 		}
 
 		@RequestMapping(value = "/add-todo", method = RequestMethod.POST)
-		public String handleTodoRequest(ModelMap model, Todo todo) {
+		public String handleTodoRequest(ModelMap model, @Valid Todo todo, BindingResult result ) {
+			if(result.hasErrors()) {
+				return "add-todo";
+			}
+
 			service.addTodo("Gabriel", "", new Date(), false);
 			model.clear();
 			return "redirect:list-todos";
--- src/main/webapp/WEB-INF/views/add-todo.jsp
+++ src/main/webapp/WEB-INF/views/add-todo.jsp
@@ -13,13 +13,19 @@
 		<form:form method="POST" commandName="todo">
 		
 		<fieldset class="form-group">
+
 			<form:label path="desc">Description:</form:label>
 			<form:input type="text" path="desc" class="form-control"
 			required="required"/>
+			<form:errors path="desc" cssClass="text-warning"/>
+			<BR/>
 
 			<form:label path="targetDate">Target Date:</form:label>
 			<form:input type="date" path="targetDate" class="form-control"
 			required="required"/>
+			<form:errors path="targetDate" cssClass="text-warning"/>
+			<BR/>
+
 		</fieldset>
 
 			<input class="btn btn-success" type="submit" value="Add"/>
