62af77563d8a5aabedb801de4c1b9161af12383c
Gabriel Müller <gabrielgoetzmuller@gmail.com>
2021-04-27 13:58:14 -0300

    initBinder for the date input format

--- src/main/java/com/in28minutes/todo/TodoController.java
+++ src/main/java/com/in28minutes/todo/TodoController.java
@@ -1,13 +1,17 @@
 package com.in28minutes.todo;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +25,13 @@ public class TodoController {
 	
 		@Autowired
 		TodoService service;
+		
+		@InitBinder
+		protected void initBinder(WebDataBinder binder) {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			binder.registerCustomEditor(Date.class, new CustomDateEditor(
+					dateFormat, false, 10));
+		}
 
 		@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
 		public String listTodos(ModelMap model) {
