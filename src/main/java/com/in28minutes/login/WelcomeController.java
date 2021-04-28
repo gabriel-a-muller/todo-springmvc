3b79ce84e813b17450248c4ae36cf695fc916917
Gabriel Müller <gabrielgoetzmuller@gmail.com>
2021-04-28 12:45:56 -0300

    Logout Feature
    Improved spring secutiry by removing any hardcoded part

--- src/main/java/com/in28minutes/login/WelcomeController.java
+++ src/main/java/com/in28minutes/login/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.login;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class WelcomeController {
 
 		@RequestMapping(value = "/", method = RequestMethod.GET)
-		public String showLoginPage(ModelMap model) {
-			model.put("name", "Gabriel");
+		public String showWelcomePage(ModelMap model) {
+			model.put("name", retrieveLoggedInUser());
 			return "welcome";
+		}
+		
+		private String retrieveLoggedInUser() {
+			Object principal = SecurityContextHolder.getContext()
+					.getAuthentication().getPrincipal();
+			
+			if (principal instanceof UserDetails)
+				return ((UserDetails) principal).getUsername();
+
+			return principal.toString();
 		}
 }
--- src/main/java/com/in28minutes/todo/TodoController.java
+++ src/main/java/com/in28minutes/todo/TodoController.java
@@ -7,6 +7,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -40,7 +42,13 @@ public class TodoController {
 		}
 
 		private String retrieveLoggedInUser() {
-			return "Gabriel";
+			Object principal = SecurityContextHolder.getContext()
+					.getAuthentication().getPrincipal();
+			
+			if (principal instanceof UserDetails)
+				return ((UserDetails) principal).getUsername();
+
+			return principal.toString();
 		}
 
 		@RequestMapping(value = "/add-todo", method = RequestMethod.GET)
